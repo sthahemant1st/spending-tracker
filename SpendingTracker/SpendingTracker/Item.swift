ee523259789adb41e0c3a881aff460c62e7dec9f
Hemant Shrestha <sthahemant1st@gmail.com>
2025-03-20 12:57:20 +0100

    initial commit

---
+++ SpendingTracker/SpendingTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SpendingTracker
+//
+//  Created by Hemant Shrestha on 20.03.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SpendingTracker/SpendingTrackerTests/SpendingTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpendingTrackerTests.swift
+//  SpendingTrackerTests
+//
+//  Created by Hemant Shrestha on 20.03.25.
+//
+
+import Testing
+
+struct SpendingTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
