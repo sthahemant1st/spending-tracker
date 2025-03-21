f36cce7a960b4fa0b2d497eb162e093efe85c9b8
Hemant Shrestha <sthahemant1st@gmail.com>
2025-03-21 15:05:29 +0100

    UI: Dasbaboard
    - tabview setup in dashboard
    - added basic UI in homescreen like expence card view

--- SpendingTracker/SpendingTrackerApp.swift
+++ SpendingTracker/SpendingTrackerApp.swift
@@ -25,7 +25,7 @@ struct SpendingTrackerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DashboardScreen()
         }
         .modelContainer(sharedModelContainer)
     }
